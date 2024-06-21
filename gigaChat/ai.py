504a016663de5542f49bd03f6737561dbf9bbdff
Dmitry <dimgrib004googl@gmail.com>
2024-06-21 17:19:31 +0300

    pre beta 1.06

--- gigaChat/ai.py
+++ gigaChat/ai.py
@@ -3,7 +3,7 @@ from langchain_community.chat_models.gigachat import GigaChat
 import os
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import DirectoryLoader
 from chromadb.config import Settings
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings.gigachat import GigaChatEmbeddings
@@ -23,32 +23,4 @@ def generate(content):
     return res.content
 
 
-# Получаем текущую директорию, в которой находится исполняемый файл
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Переходим на уровень выше и заходим в папку "documents"
-documents_dir = os.path.join(current_dir, "documents").replace('\\gigaChat', '').replace('\\', '/')
-
-# Находим все txt файлы в папке "documents"
-txt_files = [f for f in os.listdir(documents_dir) if f.endswith(".txt")]
-
-for txt_file in txt_files:
-    txt_file_path = f"{documents_dir}/{txt_file}"
-    loader = TextLoader(txt_file_path, encoding='utf-8')
-    documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200,
-    )
-    documents = text_splitter.split_documents(documents)
-    print(f"Total documents: {len(documents)}")
-embeddings = GigaChatEmbeddings(
-    credentials=API_KEY, verify_ssl_certs=False
-)
-db = Chroma.from_documents(
-    documents,
-    embeddings,
-    client_settings=Settings(anonymized_telemetry=False),
-)
 docs = db.similarity_search(input(), k=4)
-print(len(docs))
